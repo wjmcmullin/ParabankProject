1c555030e90a91e90bd4d8eabe3985246af5b76c
Collin Smith <csmith@parasoft.com>
2015-04-14 14:11:22 -0700

    commit.

---
+++ app/src/main/java/com/parabank/parasoft/app/android/Constants.java
@@ -0,0 +1,10 @@
+package com.parabank.parasoft.app.android;
+
+public interface Constants {
+    long NO_USER = -1;
+
+    String PREFS_PARABANK = "parabankConnectionSettings";
+    String PREFS_PARABANK_PROTOCOL = "protocol";
+    String PREFS_PARABANK_HOST = "host";
+    String PREFS_PARABANK_PORT = "port";
+}
