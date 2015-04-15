89a93b7ea6ecf81c6835857787349b3df5e8723d
Collin Smith <csmith@parasoft.com>
2015-04-15 16:37:13 -0700

    It should be working, however the "onFailure" is being called when it's a success >.<

--- app/src/main/java/com/parabank/parasoft/app/android/Connection.java
+++ app/src/main/java/com/parabank/parasoft/app/android/Connection.java
@@ -1,17 +1,32 @@
 package com.parabank.parasoft.app.android;
 
+import android.util.Log;
+
 public final class Connection {
     public static String generateAccountInfoURL(String host, String port, String accountNum) {
         String url = "http://" + host + ":" + port + "/parabank/services/bank/customers/"
                 + accountNum + "/accounts?_type=json";
 
-        return url;
+        return url.replaceAll("\\s", "%20");
     }
 
     public static String generateLoginURL(String host, String port, String uname, String password) {
         String url = "http://" + host + ":" + port + "/parabank/services/bank/login/"
                 + uname + "/" + password + "?_type=json";
 
-        return url;
+        return url.replaceAll("\\s", "%20");
+    }
+
+    public static String generateUpdateURL(String host, String port, String accountNumber,
+                                           String firstName, String lastName, String address, String city, String state,
+                                           String zipCode, String phoneNumber, String socialSecurity, String uname,
+                                           String password) {
+
+        String url = "http://" + host + ":" + port + "/parabank/services/bank/customers/update/" + accountNumber + "/"
+                + firstName + "/" + lastName + "/" + address + "/" + city + "/" + state + "/"
+                + zipCode + "/" + phoneNumber + "/" + socialSecurity + "/" + uname + "/" + password
+                + "?_type=json";
+
+        return url.replaceAll("\\s", "%20");
     }
 }
--- app/src/main/java/com/parabank/parasoft/app/android/EditAccountInfoActivity.java
+++ app/src/main/java/com/parabank/parasoft/app/android/EditAccountInfoActivity.java
@@ -1,9 +1,15 @@
 package com.parabank.parasoft.app.android;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -18,6 +24,9 @@ import org.apache.http.Header;
 import org.json.JSONObject;
 
 import static com.parabank.parasoft.app.android.Constants.INTENT_USER;
+import static com.parabank.parasoft.app.android.Constants.PREFS_PARABANK;
+import static com.parabank.parasoft.app.android.Constants.PREFS_PARABANK_HOST;
+import static com.parabank.parasoft.app.android.Constants.PREFS_PARABANK_PORT;
 
 public class EditAccountInfoActivity extends Activity implements View.OnClickListener {
     private User originalUser;
@@ -79,6 +88,18 @@ public class EditAccountInfoActivity extends Activity implements View.OnClickLis
 
     @Override
     public void onClick(View v) {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(etUsername.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(etPassword.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(etFirstName.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(etLastName.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(etPhoneNumber.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(etSSN.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(etStreet.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(etCity.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(etState.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(etZipCode.getWindowToken(), 0);
+
         switch(v.getId()) {
             case R.id.btnRejectChanges:
                 setResult(RESULT_CANCELED);
@@ -110,19 +131,65 @@ public class EditAccountInfoActivity extends Activity implements View.OnClickLis
     }
 
     private void updateAccountInfo(final User newUser) {
-        LinearLayout llProgressBar = (LinearLayout)findViewById(R.id.llProgressBar);
-        llProgressBar.setVisibility(View.VISIBLE);
+        //LinearLayout llProgressBar = (LinearLayout)findViewById(R.id.llProgressBar);
+        //llProgressBar.setVisibility(View.VISIBLE);
+        final ProgressDialog loadingDialog = new ProgressDialog(this);
+        loadingDialog.setMessage(getString(R.string.updating_contact_info));
+        loadingDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        loadingDialog.setCancelable(true);
+        loadingDialog.show();
 
-        String updateURL = "";
+        SharedPreferences preferences = getSharedPreferences(PREFS_PARABANK, MODE_PRIVATE);
+        //String protocol = preferences.getString(PREFS_PARABANK_PROTOCOL, getResources().getString(R.string.example_protocol));
+        String host = preferences.getString(PREFS_PARABANK_HOST, getResources().getString(R.string.example_host));
+        String port = preferences.getString(PREFS_PARABANK_PORT, getResources().getString(R.string.example_port));
+
+        String updateURL = Connection.generateUpdateURL(host, port, Long.toString(newUser.getCustomer().getId()),
+                newUser.getCustomer().getFirstName(), newUser.getCustomer().getLastName(),
+                newUser.getCustomer().getAddress().getStreet(), newUser.getCustomer().getAddress().getCity(),
+                newUser.getCustomer().getAddress().getState(), newUser.getCustomer().getAddress().getZipCode(),
+                newUser.getCustomer().getPhoneNumber(), newUser.getCustomer().getSsn(),
+                newUser.getUsername(), newUser.getPassword());
 
         AsyncHttpClient client = new AsyncHttpClient();
         client.post(updateURL, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                if (!loadingDialog.isShowing()) {
+                    return;
+                }
+
+                loadingDialog.dismiss();
+
                 Intent i = new Intent();
                 i.putExtra(INTENT_USER, newUser);
                 setResult(RESULT_OK, i);
                 finish();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                onFailure(statusCode, headers, throwable.getMessage(), throwable);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                if (!loadingDialog.isShowing()) {
+                    return;
+                }
+
+                loadingDialog.dismiss();
+
+                AlertDialog.Builder errorDialog = new AlertDialog.Builder(EditAccountInfoActivity.this);
+                errorDialog.setTitle(R.string.dialog_update_account_info_failed_title);
+                errorDialog.setMessage(String.format("%d - %s", statusCode, responseString.isEmpty() ? throwable.getMessage() : responseString));
+                errorDialog.setPositiveButton(R.string.global_action_okay, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //...
+                    }
+                });
+
+                errorDialog.show();
             }
         });
     }
